5a62fe570af912d0a613db9ee24dc4738bfd032e
Junior1999-Cangui <65523972+Junior1999-Cangui@users.noreply.github.com>
2020-06-04 11:45:44 -0500

    Código inicial

---
+++ Paradigma_Poo.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paradigma_poo;
+
+/**
+ *
+ * @author Junior
+ */
+import java.util.Scanner;
+public class Paradigma_Poo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner objeto=new Scanner(System.in);
+        System.out.println("          UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE             ");
+        System.out.println("Autor: Junior Steven Cangui Toapanta"); 
+        System.out.println("NRC:7450");
+        System.out.println("Fecha: 04/06/2020");
+        System.out.println("Tema: PARADIGMA DE LA POO\n");
+        
+        System.out.println("          REQUERIMIENTOS\n");
+        System.out.println("Ingrese requerimientos funcionales");
+        System.out.println("1.");
+        String f1=objeto.nextLine (); 
+        System.out.println("2.");
+        String f2=objeto.nextLine (); 
+        System.out.println("3.");
+        String f3=objeto.nextLine (); 
+        System.out.println("4.");
+        String f4=objeto.nextLine (); 
+        System.out.println("5.");
+        String f5=objeto.nextLine (); 
+        
+        System.out.println("Ingrese requerimientos no funcionales:");
+        System.out.println("1.");
+        String nf1=objeto.nextLine ();
+        System.out.println("2.");
+        String nf2=objeto.nextLine ();
+
+        System.out.println("       FASE DE DISEÑO ORIENTADO A OBJETOS\n");
+        System.out.println("Ingrese Objetos mundo real:");
+        System.out.println("1.");
+        String o1=objeto.nextLine (); 
+        System.out.println("2.");
+        String o2=objeto.nextLine (); 
+        System.out.println("3.");
+        String o3=objeto.nextLine (); 
+        System.out.println("4.");
+        String o4=objeto.nextLine (); 
+        System.out.println("5.");
+        String o5=objeto.nextLine (); 
+        
+        System.out.println("Ingrese Objetos mundo informatico:");
+        System.out.println("1.");
+        String oi1=objeto.nextLine (); 
+        System.out.println("2.");
+        String oi2=objeto.nextLine (); 
+        System.out.println("3.");
+        String oi3=objeto.nextLine (); 
+        System.out.println("4.");
+        String oi4=objeto.nextLine (); 
+        System.out.println("5.");
+        String oi5=objeto.nextLine (); 
+        
+        System.out.println("Ingrese Atributos:");
+        System.out.println("Objeto 1:");
+        String a1=objeto.nextLine (); 
+        System.out.println("Objeto 2:");
+        String a2=objeto.nextLine (); 
+        System.out.println("Objeto 3:");
+        String a3=objeto.nextLine (); 
+        System.out.println("Objeto 4:");
+        String a4=objeto.nextLine (); 
+        System.out.println("Objeto 5:");
+        String a5=objeto.nextLine (); 
+        
+        System.out.println("Ingrese Servicios:");
+        System.out.println("Objeto 1:");
+        String s1=objeto.nextLine (); 
+        System.out.println("Objeto 2:");
+        String s2=objeto.nextLine (); 
+        System.out.println("Objeto 3:");
+        String s3=objeto.nextLine (); 
+        System.out.println("Objeto 4:");
+        String s4=objeto.nextLine (); 
+        System.out.println("Objeto 5:");
+        String s5=objeto.nextLine (); 
+        
+        System.out.println("         CARACTERÍSTICAS DE LA PROGRAMACIÓN ORIENTADA A OBJETOS\n");
+        System.out.println("FASE DE DISEÑO ORIENTADO A OBJETOS\n");
+        System.out.println("Ingrese Clase de objeto:");
+        System.out.println("Objeto 1:");
+        String c1=objeto.nextLine (); 
+        System.out.println("Objeto 2:");
+        String c2=objeto.nextLine (); 
+        System.out.println("Objeto 3:");
+        String c3=objeto.nextLine (); 
+        System.out.println("Objeto 4:");
+        String c4=objeto.nextLine (); 
+        System.out.println("Objeto 5:");
+        String c5=objeto.nextLine (); 
+
+        System.out.println("           GRACIAS POR SU ATENCION            ");
+    }
+    
+}
